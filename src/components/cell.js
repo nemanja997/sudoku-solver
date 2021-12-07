959b6ef871273e1f7380e84bae860fc8eda01a8b
Nemanja Kazic <nemanja.kazic@vast.com>
2021-12-07 13:54:10 +0100

    Refactor Cell component to function with hooks

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,47 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-export default class Cell extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            inputNumber: 0
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-    async componentDidMount() {
-        await this.setState({
-            value: this.props.value,
-            inputNumber: this.props.position.x * 9 + this.props.position.y
-        });
-    }
-    async handleChange(event) {
+export default function Cell (props) {
+    const [value, setValue] = useState('');
+    const [inputNumber, setInputNumber] = useState(0);
+
+    useEffect(() => {
+        setValue(props.value);
+        setInputNumber(props.position.x * 9 + props.position.y)
+    }, [])
+
+    const handleChange = async (event) => {
         const number = parseInt(event.target.value);
-        const value = Number.isNaN(number) ? '' : number;
-        await this.setState({ value });
-        this.props.setValue(this.props.position, value);
+        setValue(Number.isNaN(number) ? '' : number);
+        props.setValue(props.position, value);
 
         if (!Number.isNaN(number)) {
             // focus nex cell
-            const nextCell = document.querySelector(`input[name=cell-${this.state.inputNumber + 1}]`);
+            const nextCell = document.querySelector(`input[name=cell-${inputNumber + 1}]`);
             if (nextCell !== null) {
                 nextCell.focus();
             }
         }
     }
 
-    render () {
-        return (
-            <div className="text-4xl cell">
-                <input
-                    className="w-full h-full text-center bg-gray-800 focus:bg-indigo-900 focus:outline-none"
-                    type="text"
-                    name={`cell-${this.state.inputNumber}`}
-                    tabIndex={this.state.inputNumber + 1}
-                    maxLength="1"
-                    value={this.state.value}
-                    onInput={this.handleChange}/>
-            </div>
-        )
-    }
+    return (
+        <div className="text-4xl cell">
+            <input
+                className="w-full h-full text-center bg-gray-800 focus:bg-indigo-900 focus:outline-none"
+                type="text"
+                name={`cell-${inputNumber}`}
+                tabIndex={inputNumber + 1}
+                maxLength="1"
+                value={value}
+                onInput={handleChange}/>
+        </div>
+    )
 }
